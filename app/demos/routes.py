099c4ba2735f4d5a11ce61e2363ea404d4b8d656
Will Clarke <william.clarke@digital.justice.gov.uk>
2025-04-09 21:42:59 +0100

    remove unused routes and some forms for clarity

--- app/demos/routes.py
+++ app/demos/routes.py
@@ -17,11 +17,7 @@ from fastapi.middleware.cors import CORSMiddleware
 
 from app.demos import bp
 from app.demos.forms import (
-    AutocompleteForm,
-    BankDetailsForm,
-    ConditionalRevealForm,
     CreateAccountForm,
-    KitchenSinkForm,
 )
 
 model = GPT4All("Meta-Llama-3-8B-Instruct.Q4_0.gguf")
@@ -81,43 +77,9 @@ chatbot_interface = gr.ChatInterface(
     )
 )
 
-@bp.route("/components", methods=["GET"])
-def components():
-    components = os.listdir("app/demos/govuk_components")
-    components.sort()
-
-    return render_template("components.html", components=components)
-
-
-@bp.route("/components/<string:component>", methods=["GET"])
-def component(component):
-    try:
-        with open(
-            f"app/demos/govuk_components/{component}/{component}.yaml"
-        ) as yaml_file:
-            fixtures = yaml.safe_load(yaml_file)
-    except FileNotFoundError:
-        raise NotFound
-
-    return render_template(
-        "component.html",
-        component=component,
-        fixtures=fixtures,
-    )
-
-
 @bp.route("/forms", methods=["GET"])
 def forms():
     return render_template("forms.html")
-
-
-@bp.route("/forms/bank-details", methods=["GET", "POST"])
-def bank_details():
-    form = BankDetailsForm()
-    if form.validate_on_submit():
-        flash("Demo form successfully submitted", "success")
-        return redirect(url_for("demos.forms"))
-    return render_template("bank_details.html", form=form)
 
 
 @bp.route("/forms/create-account", methods=["GET", "POST"])
@@ -127,33 +89,6 @@ def create_account():
         flash("Demo form successfully submitted", "success")
         return redirect(url_for("demos.forms"))
     return render_template("create_account.html", form=form)
-
-
-@bp.route("/forms/kitchen-sink", methods=["GET", "POST"])
-def kitchen_sink():
-    form = KitchenSinkForm()
-    if form.validate_on_submit():
-        flash("Demo form successfully submitted", "success")
-        return redirect(url_for("demos.forms"))
-    return render_template("kitchen_sink.html", form=form)
-
-
-@bp.route("/forms/conditional-reveal", methods=["GET", "POST"])
-def conditional_reveal():
-    form = ConditionalRevealForm()
-    if form.validate_on_submit():
-        flash("Demo form successfully submitted", "success")
-        return redirect(url_for("demos.forms"))
-    return render_template("conditional_reveal.html", form=form)
-
-
-@bp.route("/forms/autocomplete", methods=["GET", "POST"])
-def autocomplete():
-    form = AutocompleteForm()
-    if form.validate_on_submit():
-        flash("Demo form successfully submitted", "success")
-        return redirect(url_for("demos.forms"))
-    return render_template("autocomplete.html", form=form)
 
 @bp.route("/chatbot", methods=["GET"])
 def new_page():
