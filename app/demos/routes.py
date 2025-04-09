f862cef69959dcd0bbd97ada9adcaa3370fd127f
Alex Sheppard <alexander.sheppard@digital.justice.gov.uk>
2025-04-09 17:01:57 +0100

    Slight change to prompt

--- app/demos/routes.py
+++ app/demos/routes.py
@@ -23,41 +23,48 @@ from app.demos.forms import (
     KitchenSinkForm,
 )
 
-model = GPT4All("Meta-Llama-3-8B-Instruct.Q4_0.gguf")
-chatbot_model = pipeline("text-generation", model="gpt2")
+
 
 def query(input, history):
     system_prompt = """Act as if you are a government official who needs 
     to determine the financial eligibility for legal aid, as defined in the
      Lord Chancellor's Guidance, for an application involving the user, for 
      legal aid. 
+     
+     Do not give a definite eligibility result, just an estimate. Always say that the information 
+     will need to be reviewed by a caseworker who will make the final assessment. 
 
      Obtain the information in a series of easy to understand questions. 
      Start by checking which parts of the means assessment are relevant, based on 
      the status of the applicant and their case. Then ask for the amounts of income 
      and outgoings. Finally, give them an estimate of whether they would likely be 
      eligible for legal aid or not based on the information they have provided. 
+     
+     Avoid legal jargon. 
 
      Ask one question at a time.
 
-     Make it clear that you are not a real person.
+     Make it clear that you are not a real person once at the start of the conversation.
 
-     Do not give a definite eligibility result, just an estimate. 
-
-     Always say that the information will need to be reviewed by a caseworker who will make the final assessment. 
-
-     Avoid legal jargon. 
-
-     Don't mention the Lord Chancellor's Guidance by name. 
-     
-     Chat history:
+     Once you have made your estimate, do not ask any more questions. 
      """
+
+    chat_history = ""
+
+    for message in history:
+        chat_history += f"{message['role']}: {message['content']}\n"
+
+    system_prompt = chat_history + system_prompt
+
+    print(system_prompt)
 
     with model.chat_session(system_prompt=system_prompt):
         return model.generate(prompt=input,
                               max_tokens=240)
 
+    print(f"current_chat_session after message {model.current_chat_session}")
 
+model = GPT4All("Meta-Llama-3-8B-Instruct.Q4_0.gguf")
 # Create Gradio chatbot interface
 chatbot_interface = gr.ChatInterface(
     fn=query,
